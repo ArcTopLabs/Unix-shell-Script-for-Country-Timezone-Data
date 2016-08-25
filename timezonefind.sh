1a3f535fa3778b8200663949afca0ae58de66e56
dushyantfst <dushyant_fst@yahoo.com>
2016-08-25 22:18:29 +0530

    Unix shell script
    Shell script to call masterdatanode

---
+++ timezonefind.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#please make this make file executable using : chmod u+x .....  
+echo "Call to 'demo' App having 'timezone' Table find using GET"
+curl -H "access_token : Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO" -H "Content-Type : application/json" http://api.masterdatanode.com/demo/timezone/find?Country=India
